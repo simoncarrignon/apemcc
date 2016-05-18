02c50302cc4bfe8b4ece4a7619d42b9371331e28
Simon <simon.carrignon@gmail.com>
2016-05-18 18:58:03 +0200

    First R analysis File

---
+++ apemccAnalysis.R
@@ -0,0 +1,28 @@
+
+emp=read.csv("data/dres.csv")
+boxplot(emp$exterior_diam ~ emp$site)
+model=read.csv("result.csv") ;  
+model=model[model$time >= max(model$time)-1000,]; 
+model=model[order(model$dist),];
+
+
+
+png("CYDS.png")
+boxplot(model$exterior_diam ~ model$dist,ylab="exterior_rim",xlab="workshop",xaxt="n")
+axis(1,labels=levels(model$workshop),at=1:length(unique(model$dist)))
+dev.off()
+
+png("CNDS.png")
+boxplot(model$exterior_diam ~ model$dist,ylab="exterior_rim",xlab="workshop",xaxt="n")
+axis(1,labels=levels(model$workshop),at=1:length(unique(model$dist)))
+dev.off()
+
+png("CYDL.png")
+boxplot(model$exterior_diam ~ model$dist,ylab="exterior_rim",xlab="workshop",xaxt="n")
+axis(1,labels=levels(model$workshop),at=1:length(unique(model$dist)))
+dev.off()
+
+png("CNDL.png")
+boxplot(model$exterior_diam ~ model$dist,ylab="exterior_rim",xlab="workshop",xaxt="n")
+axis(1,labels=levels(model$workshop),at=1:length(unique(model$dist)))
+dev.off()
