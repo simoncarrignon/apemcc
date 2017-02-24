3e07ad3d6030546d2c8dac40e117bb26880b4920
Maria <mcotsar@gmail.com>
2017-02-24 17:37:38 +0100

    aaa¨

--- scriptRunAll.sh
+++ scriptRunAll.sh
@@ -1,7 +1,7 @@
-time=$1
+time=10000
 for m in "HT" "VT" "HTD";
 do
 	echo "Running simulation for model $m"
 	mkdir $m;
-	for i in {1..500}; do ./apemcc.py -w 5 -t $time -f "$m"/"$m"_"$i" -m "$m" >> $m/log_"$m" ; done ; 
+	for i in {1..100}; do ./apemcc.py -w 5 -t $time -f "$m"/"$m"_"$i" -m "$m" >> $m/log_"$m" ; done ; 
 done
