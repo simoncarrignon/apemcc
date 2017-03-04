aad5c4ad140a3a5e78c5013f72d789cab1b1f53a
Simon <simon.carrignon@gmail.com>
2017-03-04 15:11:45 +0100

    modify script to create one folder per expe

--- scriptRunAll.sh
+++ scriptRunAll.sh
@@ -1,8 +1,12 @@
 #script to run expe 
 time=30001
+exp=$1
+mkdir $exp
+cd $exp
 for m in "HT" "VT" "HTD";
 do
 	echo "Running simulation for model $m"
 	mkdir $m;
 	for i in {1..100}; do echo sim$i ; ./apemcc.py -w 5 -t $time -f "$m"/"$m"_"$i" -m "$m" >> $m/log_"$m" ; done ; 
 done
+cd ..
