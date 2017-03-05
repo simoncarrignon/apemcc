b9ea6d89fc42b82ad20c44133ae2766b62b4b3e3
Simon <simon.carrignon@gmail.com>
2017-03-05 20:04:44 +0100

    add min and max limit of size

--- apemcc.py
+++ apemcc.py
@@ -32,11 +32,13 @@ class Workshop(object):
     id=""
     all_measures={}
     prod_rate=5
-    mutation_power=.1
+    mutation_power=.005
+    world_lim={}
 
     #This function allow us to create a new workshop 
-    def __init__(self, id, dist,all_measures,prod_rate):
+    def __init__(self, id, dist,all_measures,prod_rate,world_lim):
         self.all_measures=all_measures
+        self.world_lim=world_lim
         self.id=id
         self.dist=dist
         print('New workshop called '+self.id+" at : "+str(self.dist)+" km")
@@ -71,13 +73,27 @@ class Workshop(object):
             up=-1 #increase or decrease the value
             if(random.randint(0,1)):up=1 #randomly  increase or decrease the size
             self.all_measures[measure]["mean"] = self.all_measures[measure]["mean"] + self.all_measures[measure]["mean"]* self.mutation_power  *up
+            while self.all_measures[measure]["mean"] > (self.world_lim[measure]["max"]*2) or self.all_measures[measure]["mean"] < (self.world_lim[measure]["min"]*.5):
+                if self.all_measures[measure]["mean"] > (self.world_lim[measure]["max"]*2):
+                    up=-1
+                else :
+                    up = 1 
+                self.all_measures[measure]["mean"] = self.all_measures[measure]["mean"] + self.all_measures[measure]["mean"]* self.mutation_power  *up
+            #self.all_measures[measure]["sd"] = self.all_measures[measure]["sd"]+  self.all_measures[measure]["sd"]*self.mutation_power*.01 *up # + random.random()*self.all_measures["exterior_diam"]["sd"]-self.all_measures["exterior_diam"]["sd"]
 
     def copy(self,ws2):
         for measure in self.all_measures:
             up=-1
             if(random.randint(0,1)):up=1
-            self.all_measures[measure]["mean"] = ws2.all_measures[measure]["mean"] +  self.all_measures[measure]["mean"]*self.mutation_power  *up
-            self.all_measures[measure]["sd"] = ws2.all_measures[measure]["sd"]+  self.all_measures[measure]["sd"]*self.mutation_power  *up # + random.random()*self.all_measures["exterior_diam"]["sd"]-self.all_measures["exterior_diam"]["sd"]
+            self.all_measures[measure]["mean"] = ws2.all_measures[measure]["mean"] #+  self.all_measures[measure]["mean"]*self.mutation_power  *up
+            self.all_measures[measure]["sd"] = ws2.all_measures[measure]["sd"]
+            #while self.all_measures[measure]["mean"] > self.world_lim[measure]["max"] or self.all_measures[measure]["mean"] < self.world_lim[measure]["min"]:
+            #    if self.all_measures[measure]["mean"] > self.world_lim[measure]["max"]:
+            #        up=-1
+            #    else :
+            #        up = 1 
+            #    self.all_measures[measure]["mean"] = self.all_measures[measure]["mean"] + self.all_measures[measure]["mean"]* self.mutation_power  *up
+            #self.all_measures[measure]["sd"] = ws2.all_measures[measure]["sd"]+  self.all_measures[measure]["sd"]*.0001 *up # + random.random()*self.all_measures["exterior_diam"]["sd"]-self.all_measures["exterior_diam"]["sd"]
 
     #def dist(self,ws2):
     #    return(mat_dist[self.id,ws2.id])
@@ -155,16 +171,33 @@ def main(argv):
     #    #    dist=ws+9
     #    new_ws= Workshop('ws_'+str(ws),dist,{"exterior_diam":{"mean":167.7+ws,"sd":12.26},"protruding_rim":{"mean":19+ws,"sd":5.6}},10)
     #    world.append(new_ws)
+    world_lim={"exterior_diam":{"min":130,"max":200},"protruding_rim":{"min":5,"max":40}, "rim_w":{"min":25,"max": 48}, "rim_w_2":{"min": 15,"max": 44}}
 
+              #mean of mean btw ws sd of mean btw ws min max136n of mean btw ws sd of mean btw ws min max
+              #exterior_diam           166.667395         4.9998310 130 200
+              #inside_diam              93.631245         1.3069177  70 140
+              #rim_h                    35.387083         0.8810068  25  48
+              #rim_w                    36.686752         1.9171952  25  48
+              #shape_w                   9.646956         0.6530906   5  14
+              #rim_inside_h             28.373472         1.1329995  20  39
+              #rim_w_2                  31.054947         1.2328019  15  44
+              #protruding_rim           18.273888         3.2735080   5  40
+
+
+ #exterior_diam    inside_diam          rim_h          rim_w        shape_w  rim_inside_h        rim_w_2 protruding_rim
+ #     11.126504       9.250002       3.004174       3.494843       1.080722       2.976005       4.216725       4.790658
+        #the mean standard deviation for every measurment
+        
+        
 
     for ws in  {"villaseca","belen","malpica","delicias","parlamento"}:
         dist=10 #this is not use in that case as the "distance" are given by the dictionnary world_dict
-        new_ws= Workshop(ws,dist,{"exterior_diam":{"mean":167.90,"sd":11.49},"protruding_rim":{"mean":18.30,"sd":6.136}, "rim_w":{"mean":37.23,"sd": 4.76}, "rim_w_2":{"mean": 31.24,"sd": 4.14}},10)
+        new_ws= Workshop(ws,dist,{"exterior_diam":{"mean":167.90,"sd":11},"protruding_rim":{"mean":18.30,"sd":5}, "rim_w":{"mean":37.23,"sd": 2.5}, "rim_w_2":{"mean": 31.24,"sd": 4}},100,world_lim)
         world.append(new_ws)
 
-    p_mu=.001 ##mutation probability 1 other 1000 .1 percent
+    p_mu=.01 ##mutation probability 1 other 1000 .1 percent
     p_copy=.01 ##probability of copy
-    d_weight=1 #weight of the distance
+    d_weight=.5 #weight of the distance
     
 ##begin of the simulation
     print "starting the simulation with copy mechanism:",model
@@ -183,7 +216,7 @@ def main(argv):
                 if(  model == "HT"):
                     proba= 1   #no effect of distance between the workshop ie everybody copy everybody with same proba of 1/100
                 elif model== "HTD":
-                    proba=((float(rel_dist)-(8.13))/((95.33)-(8.13)))*d_weight < random.random() #should be true when two workshop are close to eachother
+                    proba=((float(rel_dist)-(8.13))/((95.33)-(8.13))) < random.random()*d_weight #should be true when two workshop are close to eachother
                 elif model == "VT": 
                     proba= 0
 
