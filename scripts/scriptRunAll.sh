6273248b8fbc400c29ce95bdb2f3e011b94e3bd3
Simon <simon@elisya.org>
2018-05-01 20:00:43 +0200

    bash translation of model vs alpha

--- scripts/scriptRunAll.sh
+++ scripts/scriptRunAll.sh
@@ -7,7 +7,23 @@ do
 	echo "Running simulation for model $m"
 	mkdir "$exp"/"$m";
 	for i in {1..200}; do 
-        echo sim$i ; 
-        python model/main.py -i file -w 5 -t $time -f "$exp"/"$m"/"$m"_"$i" -m "$m"; 
+        #echo sim$i ; 
+        if [ $m == "HT" ]
+        then
+
+            a=-1
+        fi
+        if [ $m == "VT" ]
+        then
+
+            a=-0.5
+        fi
+        if [ $m == "HTD" ]
+        then
+
+            a=0
+        fi
+        echo $a
+        python model/main.py -i file -w 5 -t $time -f "$exp"/"$m"/"$m"_"$i" -m "$m" -a "$a"; 
     done ; 
 done
