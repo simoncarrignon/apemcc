8074d24e05e144e45a88f0c161c318a5aacdc728
Simon <simon@elisya.org>
2018-05-01 20:00:04 +0200

    add alpha in the main script

--- model/main.py
+++ model/main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     use='apemcc.py -w <number of Workshop> -t <time> -f <outputfile> -m <model> -i <init>\n `time` is the total number of time step of the simulation \n `init` should be in {"art","file"}\n `model` should be in {"HT","HTD","VT"}\n `outputfile` wille be use to write and store the results of the model'
 
     try:
-        opts, args = getopt.getopt(argv,"hw:t:f:m:i:",)
+        opts, args = getopt.getopt(argv,"hw:t:f:a:i:m:",)
     except getopt.GetoptError:
         print use
         sys.exit(2)
@@ -30,6 +30,8 @@ if __name__ == "__main__":
            max_time = int(arg)
         elif opt == "-f":
            outfile = str(arg)
+        elif opt == "-a":
+           alpha = float(arg)
         elif opt == "-m":
            model = str(arg)
         elif opt == "-i":
@@ -38,8 +40,9 @@ if __name__ == "__main__":
     p_mu=.001 ##mutation probability 1 other 1000 .1 percent
     p_copy=.01 ##probability of copy
     d_weight=.7 #weight of the distance
+    model="HT"
 
-    main_exp=CCSimu(n_ws,max_time,outfile,model,p_mu,p_copy,d_weight,init)
+    main_exp=CCSimu(n_ws,max_time,outfile,model,p_mu,p_copy,alpha,init)
 
     main_exp.run()
 
