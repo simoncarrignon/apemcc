a0a2da58a14e5af04e57369bb6f131cee653db5f
Simon <simon@elisya.org>
2018-05-02 18:15:18 +0200

    update help

--- model/main.py
+++ model/main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     outfile = "default"
 
 ###Folling lines using to parse the arguments from the command line
-    use='apemcc.py -w <number of Workshop> -t <time> -f <outputfile> -m <model> -i <init>\n `time` is the total number of time step of the simulation \n `init` should be in {"art","file"}\n `model` should be in {"HT","HTD","VT"}\n `outputfile` wille be use to write and store the results of the model'
+    use='apemcc.py -w <number of Workshop> -t <time> -f <outputfile> -i <init> -a <alpha> [-m <model>] \n `time` is the total number of time step of the simulation \n `init` should be in {"art","file"}\n `model` (for compatibility) should be in {"HT","HTD","VT"}\n `outputfile` will be used to write and store the results of the model' \n `
 
     try:
         opts, args = getopt.getopt(argv,"hw:t:f:a:i:m:",)
@@ -40,7 +40,6 @@ if __name__ == "__main__":
     p_mu=.001 ##mutation probability 1 other 1000 .1 percent
     p_copy=.01 ##probability of copy
     d_weight=.7 #weight of the distance
-    model="HT"
 
     main_exp=CCSimu(n_ws,max_time,outfile,model,p_mu,p_copy,alpha,init)
 
