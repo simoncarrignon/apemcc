cfb4556d7a6df48a9a0bbaa0e88f3172793ffc52
Simon Carrignon <simon.carrignon@bsc.es>
2019-01-23 19:31:30 +0100

    do some adpatation for mpi running

--- abc/abcpmc-conf.py
+++ abc/abcpmc-conf.py
@@ -9,12 +9,6 @@ from data.ceramic import *
 ### if use with MPI
 from  mpi_util import *
 
-pref=sys.argv[1] #a prefix that will be used as a folder to store the result of the ABC
-#
-
-if not os.path.exists(pref):
-    os.makedirs(pref)
-    ##careful this is not very mpi friendly
 
 
 #distance function: sum of abs mean differences, we take Y (the evidenceS) as a list of percentage of the same sie than x but with value .95. _ie_ our evidence are a theoretical case where all the goods are at 95% of the same type during all the years
@@ -51,17 +45,26 @@ data={'sd':allsds,'mean':allmeans}  #we dont use it in this expe
 eps = ExponentialEps(50, 0.00001, 20)
 prior = TophatPrior([0,0],[1,1])
 
+pref=sys.argv[1] #a prefix that will be used as a folder to store the result of the ABC
+#
 ### if used with MPI
 mpi_pool = MpiPool()
 sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist,pool=mpi_pool) 
 ###
 
+if mpi_pool.isMaster():
+    if not os.path.exists(pref):
+        os.makedirs(pref)
+    ##careful this is not very mpi friendly
+
+
 #sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist)
 
 for pool in sampler.sample(prior, eps):
-    np.savetxt(pref+"/output.txt","T:{0},eps:{1:>.4f},ratio:{2:>.4f}".format(pool.t, pool.eps, pool.ratio))
-    for i, (mean, std) in enumerate(zip(np.mean(pool.thetas, axis=0), np.std(pool.thetas, axis=0))):
-        np.savetxt(pref+"/output.txt",u"    theta[{0}]: {1:>.4f} \u00B1 {2:>.4f}".format(i, mean,std))
+    if mpi_pool.isMaster():
+        print(pref+"/output.txt","T:{0},eps:{1:>.4f},ratio:{2:>.4f}".format(pool.t, pool.eps, pool.ratio))
+        for i, (mean, std) in enumerate(zip(np.mean(pool.thetas, axis=0), np.std(pool.thetas, axis=0))):
+            print(pref+"/output.txt",u"    theta[{0}]: {1:>.4f} \u00B1 {2:>.4f}".format(i, mean,std))
 
     np.savetxt(pref+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",fmt='%1.5f',comments="")
     #np.savetxt(bias+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",header="n_agents,time,p_mu,p_copy",fmt='%1.5f',comments="")
