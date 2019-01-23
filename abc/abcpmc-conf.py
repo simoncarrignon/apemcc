900cc2d387e6c06a64d7dfc3ff63bdbd6d3fb0db
Simon Carrignon <simon.carrignon@bsc.es>
2019-01-23 19:12:31 +0100

    tentative to write the ratio in text

--- abc/abcpmc-conf.py
+++ abc/abcpmc-conf.py
@@ -7,12 +7,14 @@ from model.apemcc import CCSimu
 from data.ceramic import *
 
 ### if use with MPI
-#from abcpmc import mpi_util
+from  mpi_util import *
 
 pref=sys.argv[1] #a prefix that will be used as a folder to store the result of the ABC
 #
+
 if not os.path.exists(pref):
     os.makedirs(pref)
+    ##careful this is not very mpi friendly
 
 
 #distance function: sum of abs mean differences, we take Y (the evidenceS) as a list of percentage of the same sie than x but with value .95. _ie_ our evidence are a theoretical case where all the goods are at 95% of the same type during all the years
@@ -46,20 +48,20 @@ def postfn(theta):
 
 data={'sd':allsds,'mean':allmeans}  #we dont use it in this expe
 
-eps = LinearEps(10, 10, 0.115)
+eps = ExponentialEps(50, 0.00001, 20)
 prior = TophatPrior([0,0],[1,1])
 
 ### if used with MPI
-#mpi_pool = mpi_util.MpiPool()
-#sampler = abcpmc.Sampler(N=100, Y=data, postfn=postfn, dist=dist,pool=mpi_pool) 
+mpi_pool = MpiPool()
+sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist,pool=mpi_pool) 
 ###
 
-sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist)
+#sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist)
 
 for pool in sampler.sample(prior, eps):
-    print("T:{0},eps:{1:>.4f},ratio:{2:>.4f}".format(pool.t, pool.eps, pool.ratio))
+    np.savetxt(pref+"/output.txt","T:{0},eps:{1:>.4f},ratio:{2:>.4f}".format(pool.t, pool.eps, pool.ratio))
     for i, (mean, std) in enumerate(zip(np.mean(pool.thetas, axis=0), np.std(pool.thetas, axis=0))):
-        print(u"    theta[{0}]: {1:>.4f} \u00B1 {2:>.4f}".format(i, mean,std))
+        np.savetxt(pref+"/output.txt",u"    theta[{0}]: {1:>.4f} \u00B1 {2:>.4f}".format(i, mean,std))
 
     np.savetxt(pref+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",fmt='%1.5f',comments="")
     #np.savetxt(bias+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",header="n_agents,time,p_mu,p_copy",fmt='%1.5f',comments="")
