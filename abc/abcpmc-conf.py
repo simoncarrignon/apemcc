4de0b4d10d432ec12f33f8c9ac7b80c2acbcea56
Simon <simon.carrignon@bsc.es>
2019-01-24 10:29:35 +0100

    mpi switch

--- abc/abcpmc-conf.py
+++ abc/abcpmc-conf.py
@@ -6,8 +6,6 @@ import sys,os
 from model.apemcc import CCSimu
 from data.ceramic import *
 
-### if use with MPI
-from  mpi_util import *
 
 
 #distance function: sum of abs mean differences, we take Y (the evidenceS) as a list of percentage of the same sie than x but with value .95. _ie_ our evidence are a theoretical case where all the goods are at 95% of the same type during all the years
@@ -47,29 +45,35 @@ eps = ExponentialEps(200,25, 0.01)
 prior = TophatPrior([1000,0,-1],[30000,0.01,1])
 
 pref=sys.argv[1] #a prefix that will be used as a folder to store the result of the ABC
+mpi=bool(sys.argv[2])
+
+### if use with MPI
+if mpi : from  mpi_util import *
+
 #
-### if used with MPI
-mpi_pool = MpiPool()
-sampler = Sampler(N=500, Y=data, postfn=postfn, dist=dist,pool=mpi_pool) 
-###
+if mpi:
+    mpi_pool = MpiPool()
+    sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist,pool=mpi_pool) 
+else:
+    sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist)
 
-if mpi_pool.isMaster():
-    if not os.path.exists(pref):
-        os.makedirs(pref)
-
-
-#sampler = Sampler(N=200, Y=data, postfn=postfn, dist=dist)
+if mpi and mpi_pool.isMaster() and not os.path.exists(pref):
+    os.makedirs(pref)
+elif not os.path.exists(pref):
+    os.makedirs(pref)
 
 for pool in sampler.sample(prior, eps):
-    if mpi_pool.isMaster():
+    if mpi and mpi_pool.isMaster():
         logFile = open(pref+'/general.txt', 'a')
         logFile.write('starting eps: '+str(pool.eps)+'\n')
         logFile.close()
-        #print("T:{0},eps:{1:>.4f},ratio:{2:>.4f}".format(pool.t, pool.eps, pool.ratio))
-        #for i, (mean, std) in enumerate(zip(np.mean(pool.thetas, axis=0), np.std(pool.thetas, axis=0))):
-        #    print(u"    theta[{0}]: {1:>.4f},{2:>.4f}".format(i, mean,std))
         np.savetxt(pref+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",fmt='%1.5f',comments="")
         with open(pref+"/ratio.txt", "a") as myfile:
             myfile.write(str(pool.t)+","+str(pool.eps)+","+str(pool.ratio)+"\n")
+    else: 
+        print("T:{0},eps:{1:>.4f},ratio:{2:>.4f}".format(pool.t, pool.eps, pool.ratio))
+        for i, (mean, std) in enumerate(zip(np.mean(pool.thetas, axis=0), np.std(pool.thetas, axis=0))):
+            print(u"    theta[{0}]: {1:>.4f},{2:>.4f}".format(i, mean,std))
     #np.savetxt(bias+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",header="n_agents,time,p_mu,p_copy",fmt='%1.5f',comments="")
+        np.savetxt(pref+"/result_"+str(pool.eps)+".csv", pool.thetas, delimiter=",",fmt='%1.5f',comments="")
 
